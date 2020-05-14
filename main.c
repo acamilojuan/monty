fda76e81172ee0c45e2606afd3285f4f5c50e768
julianfrancor <julianfrancor@gmail.com>
2020-05-14 22:27:50 +0000

    updating main file monty project

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "monty.h"
 
+global_variables_t var_global;
 /**
  * main - program that prints its
  * name, followed by a new line.
@@ -10,11 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-		_error(1)
+	stack_t *stack =  NULL;
 
-	open_file(argv[1]);
-	free_nodes();
+	if (argc != 2)
+		_error1(1);
+
+	open_file(argv[1], &stack);
+	free_nodes(stack);
 	return (0);
 }
 
@@ -23,49 +26,17 @@ int main(int argc, char *argv[])
  * frees a doubly linked list dlist_t.
  * Return: void
  */
-void free_nodes(void)
+void free_nodes(stack_t *stack)
 {
 	stack_t *temp;
 
-	while (head)
+	if (stack == NULL)
+		return;
+	while (stack != NULL)
 	{
-		temp = head;
-		head = head->next;
+		temp = stack;
+		stack = stack->next;
 		/*freeing each node allocated with malloc*/
 		free(temp);
 	}
-	/*frees the last memory allocation*/
-	free(head);
-}
-
-/**
- * add_node - function that
- * adds a new node at the beginning of a doubly linked list dlist_t.
- * @n: constant int which is the payload (data) in the new node
- * Return: the address of the new element, or NULL if it failed
- */
-stack_t *add_node(const int n)
-{
-	stack_t *new_node, *first_node;
-
-	new_node = malloc(sizeof(stack_t));
-	if (!new_node)
-		_error(4);
-
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-
-/* Edge case when the doubly linked list is NULL*/
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-	first_node = *head;
-	new_node->next = first_node;
-	first_node->prev = new_node;
-	*head = new_node;
-
-	return (new_node);
 }
