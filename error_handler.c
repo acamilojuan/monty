bb173fb97a49a50d641dc954ed68363ca622eb91
julianfrancor <julianfrancor@gmail.com>
2020-05-14 22:30:04 +0000

    adding file to handle errors

--- error_handler.c
+++ error_handler.c
@@ -1,5 +1,5 @@
 #include "monty.h"
- 
+
 /**
 * _error1 - function that prints the error messages
 * depending on the error_code provided
@@ -12,44 +12,46 @@
 * @error_code: Type of error to be handled
 * Return: void.
 */
+
+
+
 void _error1(int error_code, ...)
 {
-    va_list args;
-    int line_number;
-    char *opcode_type;
-    char *file_name;
- 
-    va_start(args, error_code);
-    switch (error_code)
-    {
-        case 1:
-            printf("USAGE: monty file\n");
-            break;
-        case 2:
-            file_name = va_arg(args, char *);
-            printf("Error: Can't open file %s\n", file_name);
-            break;
-        case 3:
-            line_number =  va_arg(args, int);
-            opcode_type = va_arg(args, char *);
-            printf("L%d: unknown instruction\n", line_number, opcode_type);
-            break;
-        case 4:
-            printf("Error: malloc failed\n");
-            break;
-        case 5:
-            printf("L%d: usage: push integer\n", va_arg(args, int));
-            break;
-        case 6:
-            printf("L%d: usage: push integer\n", va_arg(args, int));
-            break;
-        default:
-            break;
-    }
-    free_nodes();
-    exit(EXIT_FAILURE);
+	va_list args;
+	int line_number;
+	char *opcode_type;
+	char *file_name;
+
+	va_start(args, error_code);
+	switch (error_code)
+	{
+		case 1:
+			printf("USAGE: monty file\n");
+			break;
+		case 2:
+			file_name = va_arg(args, char *);
+			printf("Error: Can't open file %s\n", file_name);
+			break;
+		case 3:
+			line_number =  va_arg(args, int);
+			opcode_type = va_arg(args, char *);
+			printf("L%d: unknown instruction %s\n", line_number, opcode_type);
+			break;
+		case 4:
+			printf("Error: malloc failed\n");
+			break;
+		case 5:
+			printf("L%d: usage: push integer\n", va_arg(args, int));
+			break;
+		case 6:
+			printf("L%d: usage: push integer\n", va_arg(args, int));
+			break;
+		default:
+			break;
+	}
+	exit(EXIT_FAILURE);
 }
- 
+
 /**
 * _error2 - function that prints the error messages
 * depending on the error_code provided
@@ -64,36 +66,37 @@ void _error1(int error_code, ...)
 */
 void _error2(int error_code, ...)
 {
-    va_list args;
- 
-    va_start(args, error_code);
-    line_number =  va_arg(args, int);
-    opcode_type = va_arg(args, char *);
-    switch (error_code)
-    {
-        case 7:
-            printf("L%d: can't pint, stack empty\n", va_arg(args, int));
-            break;
-        case 8:
-            printf("L%d: can't pop an empty stack\n", va_arg(args, int));
-            break;
-        case 9:
-            line_number =  va_arg(args, int);
-            opcode_type = va_arg(args, char *);
-            printf("L%d: can't %s, stack too short\n", line_number, opcode_type);
-            break;
-        case 10:
-            printf("L%d: division by zero\n", va_arg(args, unsigned int));
-            break;
-        case 11:
-            printf("L%d: can't pchar, value out of range\n", va_arg(args, int));
-            break;
-        case 12:
-            printf("L%d: can't pchar, stack empty\n", va_arg(args, int));
-            break;
-        default:
-            break;
-    }
-    free_nodes();
-    exit(EXIT_FAILURE);
+	va_list args;
+	int line_number;
+	char *opcode_type;
+
+	va_start(args, error_code);
+	line_number =  va_arg(args, int);
+	opcode_type = va_arg(args, char *);
+	switch (error_code)
+	{
+		case 7:
+			printf("L%d: can't pint, stack empty\n", va_arg(args, int));
+			break;
+		case 8:
+			printf("L%d: can't pop an empty stack\n", va_arg(args, int));
+			break;
+		case 9:
+			line_number =  va_arg(args, int);
+			opcode_type = va_arg(args, char *);
+			printf("L%d: can't %s, stack too short\n", line_number, opcode_type);
+			break;
+		case 10:
+			printf("L%d: division by zero\n", va_arg(args, unsigned int));
+			break;
+		case 11:
+			printf("L%d: can't pchar, value out of range\n", va_arg(args, int));
+			break;
+		case 12:
+			printf("L%d: can't pchar, stack empty\n", va_arg(args, int));
+			break;
+		default:
+			break;
+	}
+	exit(EXIT_FAILURE);
 }
