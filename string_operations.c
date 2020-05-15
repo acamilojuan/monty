59b0b2e48a50f74f7aea6feaf51bceba755b5cce
acamilojuan <1600@holbertonschool.com>
2020-05-15 05:26:59 +0000

    Rotr descp fixed

--- string_operations.c
+++ string_operations.c
@@ -80,7 +80,7 @@ void _rotl(stack_t **stack, unsigned int line_number)
 }
 /**
  * _rotr - Function that rotates the stack to the bottom.
- * @stack: Double pointer to the top of the stack.
+ * @stack: Double pointer to the top of the stack
  * @line_number: line number of the opcode in the file.
  * Return: void.
  */
