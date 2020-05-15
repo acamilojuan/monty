68a88996ca4f219cae02ef65d5c7a4cd628be14a
acamilojuan <1600@holbertonschool.com>
2020-05-15 03:15:40 +0000

    Sub, Div, Mul y Mod Functions added

--- _add.c
+++ _add.c
@@ -2,7 +2,7 @@
 /**
  * _add - adds the top two elements of the stack.
  * If the stack contains less than two elements, handle error
- * with _error(8).
+ * with _error(9).
  * @stack: Double pointer to the top of the stack.
  * @line_number: line number of the opcode in the file.
  * Return: void.
@@ -14,6 +14,80 @@ void _add(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL || stack == NULL || (*stack)->next == NULL)
 		_error1(9, line_number);
 	new->next->n += new->n;
+	*stack = (*new).next;
+	(*stack)->prev = NULL;
+}
+/**
+ * _sub - subs the top two elements of the stack.
+ * If the stack contains less than two elements, handle error
+ * with _error(9).
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: line number of the opcode in the file.
+ * Return: void.
+ */
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = *stack;
+
+	if (*stack == NULL || stack == NULL || (*stack)->next == NULL)
+		_error1(9, line_number);
+	new->next->n -= new->n;
+	*stack = (*new).next;
+	(*stack)->prev = NULL;
+}
+/**
+ * _div - Divides the top two elements of the stack.
+ * If the stack contains less than two elements, handle error
+ * with _error(9).
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: line number of the opcode in the file.
+ * Return: void.
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = *stack;
+
+	if (*stack == NULL || stack == NULL || (*stack)->next == NULL)
+		_error1(9, line_number);
+	new->next->n /= new->n;
+	*stack = (*new).next;
+	(*stack)->prev = NULL;
+}
+/**
+ * _mul - Multiples the top two elements of the stack.
+ * If the stack contains less than two elements, handle error
+ * with _error(9).
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: line number of the opcode in the file.
+ * Return: void.
+ */
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = *stack;
+
+	if (*stack == NULL || stack == NULL || (*stack)->next == NULL ||
+	(*stack)->next == 0)
+		_error1(9, line_number);
+	new->next->n -= new->n;
+	*stack = (*new).next;
+	(*stack)->prev = NULL;
+}
+/**
+ * _mod - Modules the top two elements of the stack.
+ * If the stack contains less than two elements, handle error
+ * with _error(8).
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: line number of the opcode in the file.
+ * Return: void.
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new = *stack;
+
+	if (*stack == NULL || stack == NULL || (*stack)->next == NULL
+	(*stack)->next == 0)
+		_error1(9, line_number);
+	new->next->n %= new->n;
 	*stack = (*new).next;
 	(*stack)->prev = NULL;
 }
